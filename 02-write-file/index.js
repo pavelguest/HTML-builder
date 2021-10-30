170ad369ed97bcdadf45e9cbc017f2b038d49cbd
Pavel <pavelguest@yandex.ru>
2021-10-30 20:38:25 +0300

    feat: add solving second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const fs = require('fs');
+const readline = require('readline');
+const { stdin, stdout } = process;
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const inputText = fs.createWriteStream(path.join(__dirname, 'text.txt'))
+
+stdout.write(`Please enter any text:\n`);
+stdin.on('data', chunk => {
+  inputText.write(chunk)
+})
+rl.on('line', (line) => {
+    switch (line) {
+      case 'exit':
+        rl.close(); }
+    })
+
+rl.on('close', () => {
+  console.log(`File create! Good luck!`)
+  process.exit(0);
+    });
+
+
+
