808b544e331341dba57a18fe7035b62eb69f3a38
Pavel <pavelguest@yandex.ru>
2021-10-31 01:48:04 +0300

    fix: path for files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,32 +4,24 @@ const fs = require('fs');
 const findFolder = path.join(__dirname, 'files');
 const createFolder = path.join(__dirname, 'files-copy');
 
-fs.readdir(createFolder, (err, filesCopy) => {
-  if (err) {
-    throw err;
-  }
-  filesCopy.forEach( filesCopy => {
-    fs.unlink(createFolder + '/' + filesCopy, (err) => {
-      if (err) {
-        throw err;
-      }
-    })
-  })
-})
-
 fs.mkdir(createFolder, { recursive: true }, (err) => {
   if(err) throw err;
 });
 
+fs.readdir(createFolder, (err, filesCopy) => {
+  if (err) throw err;
+  filesCopy.forEach( filesCopyItem => {
+    fs.unlink(path.join(__dirname, 'files-copy', filesCopyItem), (err) => {
+      if (err) throw err;
+    })
+  })
+})
+
 fs.readdir(findFolder, (err, files) => {
-  if (err) {
-    throw err;
-  }
-  files.forEach( files => {
-    fs.copyFile(findFolder + '/' + files, createFolder + '/' + files, (err) => {
-      if (err) {
-        throw err;
-      }
+  if (err) throw err;
+  files.forEach( filesItem => {
+    fs.copyFile(path.join(__dirname, 'files', filesItem), path.join(__dirname, 'files-copy', filesItem), (err) => {
+      if (err) throw err;
     });
   })
   console.log(`Files copied successfully`)
